8e44fb9016896c8d0614d9d4a7f1e4dfede5db44
nandriese <nicholas.andriese@ucf.edu>
2022-07-04 00:10:32 -0400

    Update Assignment02.py

--- Assignment02/Assignment02.py
+++ Assignment02/Assignment02.py
@@ -1,7 +1,44 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul  3 23:04:00 2022
-
-@author: ni664326
 """
+#2
+
+Yo = 2
+I = 2.0
+Love = 10j
+Python = "2 Cool for School"
+Mucho = "True"
+
+type(Yo)
+type(I)
+type(Love)
+type(Python)
+type(Mucho)
+
+#3
+    
+A = [Yo, I, Love, Python, Mucho]
+
+#4
+
+B = "I like pie more than cake."
+
+B[:6]
+B[7:15]
+B[16:25]
+B[:6] + B[10:15] + B[20:25]
+
+#5
+x = int(input('Enter number for x here: '))
+x = 22
+
+if (x % 15 == 0):
+   print("foobar")
+elif (x % 5 == 0):
+   print("bar")
+elif (x % 3 == 0):
+    print("foo")
+elif (x % 15 != 0):
+    print("Not a multiple of 3, 5, or 15")
 

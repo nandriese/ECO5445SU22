c53a7dff55cda26dd0027243c43840e5c18668f6
nandriese <nicholas.andriese@ucf.edu>
2022-07-10 20:37:38 -0400

    Update Assignment04.py

--- Assignment04/Assignment04.py
+++ Assignment04/Assignment04.py
@@ -5,3 +5,28 @@ Created on Sat Jul  9 23:40:32 2022
 @author: ni664326
 """
 
+import numpy as np
+    
+help(np.random.uniform)
+  
+points = 1000000
+pi = np.random.uniform(0,1,points)
+
+circlepoints = 0
+squarepoints = 0
+
+for i in range(points):
+    xpoint = np.random.uniform(0,1)
+    ypoint = np.random.uniform(0,1)
+    
+    dist = (((xpoint-.5)**2) + (ypoint-.5)**2)**(1/2)
+    
+    if dist <= .5:
+        circlepoints += 1
+    
+    squarepoints += 1
+
+    
+pi = (4 *(circlepoints/squarepoints))
+
+print(pi)
